b2dc27cf180a225c695cafec3592985dd330ed68
matias <matiassoto00@outlook.com>
2025-08-20 19:18:04 -0300

    ejercicios

---
+++ TP3/2.py
@@ -0,0 +1,6 @@
+#Ej2
+
+nota_alumno : float = float(input("ingrese nota"))
+calificacion = "Aprobado" if nota_alumno >= 6 else "Desaprobado"
+
+print (calificacion)
---
+++ TP3/3.py
@@ -0,0 +1,8 @@
+#Ej3
+
+numero:float = float(input("ingrese un numero"))
+
+if (numero % 2 == 0):
+    print("Ha ingresado un numero par")
+else:
+    print("Por favor, ingrese un numero par")
